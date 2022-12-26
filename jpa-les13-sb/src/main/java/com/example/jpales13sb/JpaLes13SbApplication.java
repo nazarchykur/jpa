c79789416451f40e5ac0db6d380d608ab871d3b4
NazarChykur <nazar.chykur@emburse.com>
2022-12-26 11:41:02 +0200

    Lesson 13 - JPA in a layered architecture: spring boot

---
+++ jpa-les13-sb/src/main/java/com/example/jpales13sb/JpaLes13SbApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpales13sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaLes13SbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaLes13SbApplication.class, args);
+	}
+
+}
---
+++ jpa-les13-sb/src/test/java/com/example/jpales13sb/JpaLes13SbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpales13sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaLes13SbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
